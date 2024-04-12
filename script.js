12943dba1faf7620fdd8cba21e8721cb84b919ed
creativeact <128939729+creativeact@users.noreply.github.com>
2024-04-12 14:36:46 -0600

    Add conditional logic for updateDisplay
    Conditional logic for updateDisplay so that:
    - num1, operator, and num2 variables are assigned a value
    depending on which button the user clicks
    - input display only allows for 2 numbers total
    - input dispaly only allows operator input if num1 is not null

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
-// Declare operators
+// Declare number and operator variables
+let num1 = null;
+let num2 = null;
+let operator = null;
+
+// Declare operator functions
 const add = function(x,y) {
     return x + y;
 }
-
 const subtract = function(x,y) {
     return x + y;
 }
-
 const multiply = function(x,y) {
     return x * y;
 }
-
 const divide = function(x,y) {
     return x / y;
 }
 
-// Call update display function when number is clicked
+const inputDisplay = document.querySelector('#display-input');
+
+// Call updateDisplay to update input when number is clicked
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => {
     number.addEventListener('click', () => {
         updateDisplay(number.textContent);
+        if (num1 === null) {
+            num1 = `${number.textContent}`;
+        }
+        else if (num1 !== null && operator === null) {
+            num1 += `${number.textContent}`;
+        }
+        else if (num1 !== null && operator !== null) {
+            num2 = `${number.textContent}`;
+        }
+        else if (num1 !== null && operator !== null && num2 !== null) {
+            num2 += `${number.textContent}`;
+        }
     });
 });
 
-// Display input to user as it is updated
-const inputDisplay = document.getElementById('display-input');
+// Call updateDisplay to update input when operator is clicked
+const operatorBtns = document.querySelectorAll('.operator');
+operatorBtns.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', () => {
+        if (num1 !== null && operator === null) {
+            updateDisplay(operatorBtn.textContent);
+            // Set operator variable to the corresponding operatorBtn
+            operator = `${operatorBtn.textContent}`;
+        }
+    });
+});
 
 function updateDisplay(input) {
-    inputDisplay.textContent = input;
+    if (input === "+" || input === "-" || input === "x" || input === 
+    "/") {
+        inputDisplay.textContent +=` ${input} `;
+    }
+    else
+    inputDisplay.textContent +=`${input}`;
 }
 
 // Execute operation based on inputs
