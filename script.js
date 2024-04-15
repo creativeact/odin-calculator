ad333bd879a35c9915a57bf66b45196118d1be41
creativeact <128939729+creativeact@users.noreply.github.com>
2024-04-15 16:00:35 -0600

    Add ability to do multiple calculations
    Create new result variable to support multiple calculations
    
    Add conditional logic in the following functions to support multiple
    calculations:
    - updateInputDiplay
    - operatorBtn eventListener function
    - number eventListener function

--- script.js
+++ script.js
@@ -2,47 +2,82 @@
 let num1 = null;
 let num2 = null;
 let operator = null;
+let result = null;
 
 // Declare display elements to be updated
 const inputDisplay = document.querySelector('#input-display');
 const outputDisplay = document.querySelector('#output-display');
 
-// Call updateInpuDisplay when number is clicked
+// Assign number variables and call updateInpuDisplay when number is clicked
 const numbers = document.querySelectorAll('.number');
 numbers.forEach(number => {
+    let input = "";
     number.addEventListener('click', () => {
-        updateInputDisplay(number.textContent);
-        if (num1 === null) {
-            num1 = `${number.textContent}`;
+        input = number.textContent;
+
+        // Prevent user from using leading zeros
+        if (num1 === null && input === "0") {
+            return;
+        }
+        else if (num1 !== null && operator !== null && result === null && num2 === "0") {
+            return;
+        }
+
+        // Handle edge case for clicking number directly after operate function
+        else if (result !== null) {
+            num1 =`${input}`;
+            operator = null;
+        }
+
+        // Update num1 or num2 based on conditions
+        else if (num1 === null) {
+            num1 = `${input}`;
         }
         else if (num1 !== null && operator === null) {
-            num1 += `${number.textContent}`;
+            num1 += `${input}`;
         }
         else if (num1 !== null && operator !== null && num2 !== null) {
-            num2 += `${number.textContent}`;
+            num2 += `${input}`;
         }
         else if (num1 !== null && operator !== null) {
-            num2 = `${number.textContent}`;
+            num2 = `${input}`;
         }
+        updateInputDisplay(input);
     });
 });
 
-// Call updateInputDisplay when operator is clicked
+// Assign operator variable and call updateInputDisplay when operator is clicked
 const operatorBtns = document.querySelectorAll('.operator');
 operatorBtns.forEach(operatorBtn => {
     operatorBtn.addEventListener('click', () => {
+        input = operatorBtn.textContent;
+
+        // Prevent user from starting with operator
         if (num1 !== null && operator === null) {
-            updateInputDisplay(operatorBtn.textContent);
-            // Set operator variable to the corresponding operatorBtn
-            operator = `${operatorBtn.textContent}`;
+            updateInputDisplay(input);
+            operator = `${input}`;
+        }
+
+        // Re-assign num1 and operator for any follow-on equations
+        else if (result !== null) {
+            num1 = parseInt(result);
+            updateInputDisplay(num1);
+            updateInputDisplay(input);
+            operator = `${input}`;
         }
     });
 });
 
+// Update display with input and add a space around operators
 function updateInputDisplay(input) {
     if (input === "+" || input === "-" || input === "x" || input === 
     "/") {
         inputDisplay.textContent +=` ${input} `;
+    }
+    else if (result !== null) {
+        inputDisplay.textContent = `${input}`;
+        result = null;
+        num2 = null;
     }
     else
     inputDisplay.textContent +=`${input}`;
@@ -70,22 +105,29 @@ allClear.addEventListener('click', () => {
 function operate(num1, num2, operator) {
     let x = parseInt(num1);
     let y = parseInt(num2);
-    let result = null;
+    let solution = null;
     if (operator === '+') {
-        result = x + y;
+        solution = x + y;
     }
     else if (operator === '-') {
-        result = x - y;
+        solution = x - y;
     }
     else if (operator === 'x') {
-        result = x * y;
+        solution = x * y;
     }
     else if (operator === '/') {
-        result = x / y;
+        solution = x / y;
     }
-    updateOutputDisplay(result);
+
+    // Prevent division by 0
+    if (solution === Infinity) {
+        alert("You can't divide by 0! Clear and retry");
+    }
+    else
+    updateOutputDisplay(solution);
 }
 
-function updateOutputDisplay(result) {
-    outputDisplay.textContent = `${result}`;
+function updateOutputDisplay(solution) {
+    outputDisplay.textContent = `${solution}`;
+    result = solution;
 }
