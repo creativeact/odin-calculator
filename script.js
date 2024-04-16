f20fbecd49c72a6ce9561216da3760ee3c6225a2
creativeact <128939729+creativeact@users.noreply.github.com>
2024-04-15 21:43:37 -0600

    Add clear functionality
    Add eventListener for clear button and conditional logic to update
    variables num1, operator, or num2 after being clicked

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Declare number and operator variables
+// Declare number and operator variables to be used in operate function
 let num1 = null;
 let num2 = null;
 let operator = null;
@@ -51,22 +51,51 @@ numbers.forEach(number => {
     });
 });
 
+// Add decimal point to num1 or num2 based on conditions
 const decimalBtn = document.querySelector('#decimal-place');
 decimalBtn.addEventListener('click', () => {
     if (num1 === null) {
-        updateInputDisplay("0.")
+        num1 = "0.";
+        updateInputDisplay(num1);
     }
-})
+    else if (num1.includes(".") === false && num2 === null) {
+        num1 += ".";
+        updateInputDisplay(".");
+    }
+    else if (num1 !== null && operator !== null && num2 === null) {
+        num2 = "0.";
+        updateInputDisplay(num2);
+    }
+    else if (num2.includes(".") === false) {
+        num2 += ".";
+        updateInputDisplay(".");
+    }
+});
 
 // Pass negative flag to updateInputDisplay and reassign num variable
 const negativeBtn = document.querySelector('#negative-flag');
 negativeBtn.addEventListener('click', () => {
-    if (num1 !== null && num2 === null) {
-        num1 = parseInt(num1) * -1;
+    if (num1 !== null && operator !== null && num2 === null) {
+        return;
+    }
+
+    // Error handling for garbage values after clearing
+    else if (isNaN(parseFloat(num1)) === true) {
+        return;
+    }
+    else if (num1 !== null && num2 === "") {
+        return;
+    }
+
+    // Calculate newNum then convert to string
+    else if (num1 !== null && num2 === null) {
+        let newNum = parseFloat(num1) * -1;
+        num1 = newNum.toString();
         updateInputDisplay("negative-flag");
     }
-    else if(num1 !=null && num2 !== null && result === null) {
-        num2 = parseInt(num2) * -1;
+    else if(num1 !== null && num2 !== null && result === null) {
+        let newNum = parseFloat(num2) * -1;
+        num2 = newNum.toString();
         updateInputDisplay("negative-flag");
     }
 });
@@ -78,14 +107,17 @@ operatorBtns.forEach(operatorBtn => {
         input = operatorBtn.textContent;
 
         // Prevent user from starting with operator
-        if (num1 !== null && operator === null) {
+        if (parseFloat(num1) === NaN || num1 === undefined || num1 ==="") {
+            return;
+        }
+        else if (num1 !== null && operator === null) {
             updateInputDisplay(input);
             operator = `${input}`;
         }
-
+        
         // Re-assign num1 and operator for any follow-on equations
         else if (result !== null) {
-            num1 = parseInt(result);
+            num1 = parseFloat(result);
             updateInputDisplay(num1);
             updateInputDisplay(input);
             outputDisplay.textContent = "";
@@ -94,18 +126,43 @@ operatorBtns.forEach(operatorBtn => {
     });
 });
 
-// Update display with input and add a space around operators
+// Update display with input
 function updateInputDisplay(input) {
+
+    // Add a space around operators
     if (input === "+" || input === "-" || input === "x" || input === 
     "/") {
         inputDisplay.textContent +=` ${input} `;
     }
+
+    // Update display when + / - button is clicked
     else if (input === "negative-flag" && num2 === null) {
         inputDisplay.textContent = `${num1}`;
     }
     else if (input === "negative-flag" && num2 !== null) {
         inputDisplay.textContent = `${num1} ${operator} ${num2}`;
     }
+
+    // Prevent errors from 
+
+    // Clear input based on conditions
+    else if (input === "clear" && operator === null && isNaN(parseFloat(num2)) === true) {
+        let newNum = num1.slice(0, -1);
+        num1 = newNum;
+        inputDisplay.textContent = `${num1}`;
+    }
+    else if (input === "clear" && operator !== null && isNaN(parseFloat(num2)) === true) {
+        operator = null;
+        inputDisplay.textContent = `${num1}`;
+    }
+    else if (input === "clear" && operator !== null && num2 != null & result === null) {
+        let newNum = num2.slice(0, -1);
+        num2 = newNum;
+        inputDisplay.textContent = `${num1} ${operator} ${num2}`;
+    }
+
+
+    // If there was a previous calculation, reset variables to prepare for new operate call
     else if (result !== null) {
         inputDisplay.textContent = `${input}`;
         result = null;
@@ -116,7 +173,7 @@ function updateInputDisplay(input) {
 }
 
 // Call operate function when equal is clicked
-const equal = document.querySelector('#equal');
+const equal = document.querySelector('.equal');
 equal.addEventListener('click', () => {
     if (num1 !== null && operator !== null && num2 !== null) {
         operate(num1, num2, operator);
@@ -129,16 +186,43 @@ allClear.addEventListener('click', () => {
     num1 = null;
     operator = null;
     num2 = null;
+    result = null;
     inputDisplay.textContent = '';
     outputDisplay.textContent = '';
 });
 
+// Call updateInputDisplay with clear as input
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    if (num1 === null) {
+       return;
+    }
+    else if (result !== null) {
+        num1 = "0";
+        operator = null;
+        num2 = null;
+        inputDisplay.textContent = "0";
+        outputDisplay.textContent = '';
+        result = null;
+    }
+    else {
+        updateInputDisplay("clear");
+    }
+})
+
 // Execute operation based on inputs
 function operate(num1, num2, operator) {
-    let x = parseInt(num1);
-    let y = parseInt(num2);
+    let x = parseFloat(num1);
+    let y = parseFloat(num2);
     let solution = null;
-    if (operator === '+') {
+
+    // Prevent calculations with garbage values after clearing
+    if (isNaN(x) === true || isNaN(y) === true) {
+        return;
+    }
+
+    // Carry out operation based on operator
+    else if (operator === '+') {
         solution = x + y;
     }
     else if (operator === '-') {
